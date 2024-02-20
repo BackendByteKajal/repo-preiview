05df75d4a5436fae1a79ad54fbdef2b62ad97dab
kajal jagtap <kajal.jagtap@techalchemy.com>
2024-02-20 19:21:09 +0530

    cron add

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,12 +1,11 @@
 import { apiHandler } from "../../helpers/api/api-handler";
 
-
 export default apiHandler({
   get: hello,
 });
 
 async function hello(req, res) {
   const param = req.query.action;
- console.log("hello")
+  console.log("hello world");
   return res.status(200).json("content");
 }
